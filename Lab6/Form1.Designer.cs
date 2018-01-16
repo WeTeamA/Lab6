fba8643f82e0fb96e97b9abed7ad1b017252e41b
Смирнов Михаил Андреевич <32768288+Karnoksis@users.noreply.github.com>
2018-01-17 02:21:53 +0300

    Теперь надо исправить повторное выполнение кода после первого удачного

--- Lab6/Form1.Designer.cs
+++ Lab6/Form1.Designer.cs
@@ -29,11 +29,57 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            this.textBox_output = new System.Windows.Forms.TextBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox_input
+            // 
+            this.textBox_input.Font = new System.Drawing.Font("Consolas", 11F);
+            this.textBox_input.Location = new System.Drawing.Point(12, 12);
+            this.textBox_input.Multiline = true;
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox_input.Size = new System.Drawing.Size(567, 335);
+            this.textBox_input.TabIndex = 0;
+            this.textBox_input.TextChanged += new System.EventHandler(this.textBox_input_TextChanged);
+            // 
+            // textBox_output
+            // 
+            this.textBox_output.Font = new System.Drawing.Font("Consolas", 11F);
+            this.textBox_output.Location = new System.Drawing.Point(12, 353);
+            this.textBox_output.Multiline = true;
+            this.textBox_output.Name = "textBox_output";
+            this.textBox_output.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox_output.Size = new System.Drawing.Size(567, 158);
+            this.textBox_output.TabIndex = 1;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 523);
+            this.Controls.Add(this.textBox_output);
+            this.Controls.Add(this.textBox_input);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.TextBox textBox_output;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- Lab6/Form1.cs
+++ Lab6/Form1.cs
@@ -5,8 +5,12 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
+using System.CodeDom.Compiler;
+using Microsoft.CSharp;
 
 namespace Lab6
 {
@@ -15,6 +19,88 @@ namespace Lab6
         public Form1()
         {
             InitializeComponent();
+        }
+        int time;
+        string[] dll = new string[]
+        {"System.dll",
+         "System.Linq.dll",
+         "System.Threading.Tasks.dll",
+         "System.Windows.Forms.dll"
+        };
+
+        static Dictionary<string, string> providerOptions = new Dictionary<string, string>
+        {
+            {"CompilerVersion", "v4.0"}
+        };
+        CSharpCodeProvider provider = new CSharpCodeProvider(providerOptions); //Создаем объект компилятора
+        CompilerParameters compilerParams = new CompilerParameters //Задаем параметры компилятору
+        {
+            GenerateInMemory = true,
+            GenerateExecutable = true,
+        };
+        
+
+        /// <summary>
+        /// Компилирует
+        /// </summary>
+        public void Compile()
+        {
+
+            CompilerResults results = provider.CompileAssemblyFromSource(compilerParams, textBox_input.Text); //Получаем результат исполнения исходного кода при примененных параметрах
+
+            #region Отлов ошибок
+            if (results.Errors.HasErrors)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (CompilerError error in results.Errors)
+                {
+                    sb.AppendLine(String.Format("Error ({0}): {1}", error.ErrorNumber, error.ErrorText));
+                }
+
+                throw new InvalidOperationException(sb.ToString());
+            }
+#endregion
+
+            //Используем рефлексию для манипуляциями полученных классов и методов
+            Assembly assembly = results.CompiledAssembly; //Получаем скомпилированную сборку в объект типа Assembly
+            Type program = assembly.GetType("Lab6.Program"); //
+            MethodInfo main = program.GetMethod("Main"); //
+            main.Invoke(null, null); //Запускаем метод main
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            time++;
+            if (time >= 3)
+            {
+                Compile();
+                timer.Stop();
+                time = 0;
+            }
+        }
+
+        private void textBox_input_TextChanged(object sender, EventArgs e)
+        {
+            timer.Stop();
+            time = 0;
+            timer.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox_input.Text =
+@"namespace Lab6
+   {
+        public class Program
+        {
+            public static void Main()
+            {
+
+            }
+        }
+   }";
+            compilerParams.ReferencedAssemblies.AddRange(dll);//Добавляем библиотеки к параметрам компилятора
         }
     }
 }
