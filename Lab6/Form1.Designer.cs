33dbba64a86f86d31769c96f5e4f3ba7be96860c
cTu4 <cTu4_22@users.noreply.github.com>
2018-01-17 22:17:59 +0300

    конча

--- Lab6/Form1.Designer.cs
+++ Lab6/Form1.Designer.cs
@@ -29,44 +29,49 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.textBox_input = new System.Windows.Forms.TextBox();
             this.textBox_output = new System.Windows.Forms.TextBox();
             this.timer = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
-            // 
-            // textBox_input
-            // 
-            this.textBox_input.Font = new System.Drawing.Font("Consolas", 11F);
-            this.textBox_input.Location = new System.Drawing.Point(12, 12);
-            this.textBox_input.Multiline = true;
-            this.textBox_input.Name = "textBox_input";
-            this.textBox_input.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox_input.Size = new System.Drawing.Size(567, 335);
-            this.textBox_input.TabIndex = 0;
-            this.textBox_input.TextChanged += new System.EventHandler(this.textBox_input_TextChanged);
             // 
             // textBox_output
             // 
-            this.textBox_output.Font = new System.Drawing.Font("Consolas", 11F);
+            this.textBox_output.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.textBox_output.Font = new System.Drawing.Font("Segoe MDL2 Assets", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox_output.Location = new System.Drawing.Point(12, 353);
             this.textBox_output.Multiline = true;
             this.textBox_output.Name = "textBox_output";
+            this.textBox_output.ReadOnly = true;
             this.textBox_output.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox_output.Size = new System.Drawing.Size(567, 158);
-            this.textBox_output.TabIndex = 1;
+            this.textBox_output.Size = new System.Drawing.Size(816, 158);
+            this.textBox_output.TabIndex = 2;
             // 
             // timer
             // 
             this.timer.Interval = 1000;
             this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.Black;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(816, 325);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(591, 523);
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(840, 523);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.textBox_output);
-            this.Controls.Add(this.textBox_input);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -76,10 +81,9 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox_input;
         private System.Windows.Forms.TextBox textBox_output;
         private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- Lab6/Form1.cs
+++ Lab6/Form1.cs
@@ -12,6 +12,7 @@ using System.Reflection;
 using System.CodeDom.Compiler;
 using Microsoft.CSharp;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Lab6
 {
@@ -21,27 +22,27 @@ namespace Lab6
         {
             InitializeComponent();
         }
-        /// <summary>
-        /// Переменная для счета времени, прошедшего после написания кода (для активации компиляции)
-        /// </summary>
+        /// <summary> 
+        /// Переменная для счета времени, прошедшего после написания кода (для активации компиляции) 
+        /// </summary> 
         int time;
         string[] dll = new string[]
         {"System.dll",
-         "System.Linq.dll",
-         "System.Threading.Tasks.dll",
-         "System.Windows.Forms.dll",
-         "mscorlib.dll",
-         "System.Data.dll"
+"System.Linq.dll",
+"System.Threading.Tasks.dll",
+"System.Windows.Forms.dll",
+"mscorlib.dll",
+"System.Data.dll"
         };
 
         static Dictionary<string, string> providerOptions = new Dictionary<string, string>
-        {
-            {"CompilerVersion", "v4.0"}
-        };
+{
+{"CompilerVersion", "v4.0"}
+};
 
-        CSharpCodeProvider provider = new CSharpCodeProvider(providerOptions); //Создаем объект компилятора
+        CSharpCodeProvider provider = new CSharpCodeProvider(providerOptions); //Создаем объект компилятора 
 
-        CompilerParameters compilerParams = new CompilerParameters //Задаем параметры компилятору
+        CompilerParameters compilerParams = new CompilerParameters //Задаем параметры компилятору 
         {
             GenerateInMemory = true,
             GenerateExecutable = true,
@@ -49,9 +50,9 @@ namespace Lab6
         StringBuilder sb = new StringBuilder();
         public void Compile()
         {
-            CompilerResults results = provider.CompileAssemblyFromSource(compilerParams, textBox_input.Text); //Получаем результат исполнения исходного кода при примененных параметрах
-            
-            #region Отлов ошибок
+            CompilerResults results = provider.CompileAssemblyFromSource(compilerParams, compil + richTextBox1.Text + "}}"); //Получаем результат исполнения исходного кода при примененных параметрах 
+
+            #region Отлов ошибок 
             if (results.Errors.HasErrors)
             {
                 foreach (CompilerError error in results.Errors)
@@ -60,35 +61,26 @@ namespace Lab6
                 }
                 throw new InvalidOperationException(sb.ToString());
             }
-#endregion
+            #endregion
 
-            //Используем рефлексию для манипуляциями полученных классов и методов
-            Assembly assembly = results.CompiledAssembly; //Получаем скомпилированную сборку в объект типа Assembly
-            Type program = assembly.GetType("Lab6.Program"); //
-            MethodInfo main = program.GetMethod("Main"); //
-            
+            //Используем рефлексию для манипуляциями полученных классов и методов 
+            Assembly assembly = results.CompiledAssembly; //Получаем скомпилированную сборку в объект типа Assembly 
+            Type program = assembly.GetType("Lab6.Program"); // 
+            MethodInfo main = program.GetMethod("Main"); // 
+
             timer.Stop();
             time = 0;
 
-            main.Invoke(null, null); //Запускаем метод main
+            main.Invoke(null, null); //Запускаем метод main 
         }
 
         private void timer_Tick(object sender, EventArgs e)
         {
             time++;
-            if (time >= 4)
+            if (time >= 3)
             {
                 try
                 {
-                    
-                    // var sw = new StringWriter();
-                    // TextWriter sww = Console.Out;
-                    //  Console.SetOut(sw);
-                    // Console.SetError(sw);
-                    //Console.SetCursorPosition(0, 0);
-                    // textBox_output.Text = Console.ReadLine();
-                    //Console.WriteLine("You are travelling north at a speed of 10m/s");
-
                     using (StringWriter stringWriter = new StringWriter())
                     {
                         Console.SetOut(stringWriter);
@@ -102,7 +94,7 @@ namespace Lab6
                 {
                     timer.Stop();
                     time = 0;
-                    textBox_output.Clear();                 
+                    textBox_output.Clear();
                     textBox_output.Text = sb.ToString();
                     sb.Clear();
                 }
@@ -111,39 +103,69 @@ namespace Lab6
 
         private void textBox_input_TextChanged(object sender, EventArgs e)
         {
+
+
+        }
+        string compil = @"using System; 
+using System.Collections.Generic; 
+using System.ComponentModel; 
+using System.Data; 
+using System.Drawing; 
+using System.Linq; 
+using System.Text; 
+using System.Threading; 
+using System.Threading.Tasks; 
+using System.Windows.Forms; 
+using System.Reflection; 
+using System.CodeDom.Compiler; 
+using Microsoft.CSharp; 
+using System.IO; 
+namespace Lab6 //Скрыть от пользователя 
+{ 
+public class Program 
+{ 
+interface OutPut 
+{ 
+void Out(string x); 
+} 
+public class WriteText:OutPut 
+{ 
+public void Out(string x) 
+{ 
+Console.WriteLine(x); 
+} 
+}";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.Text =
+            @"public static void Main() 
+{ 
+WriteText s = new WriteText(); // s.Out(string) выведет строку в TextBox 
+
+}";
+            compilerParams.ReferencedAssemblies.AddRange(dll); 
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            int cur = richTextBox1.SelectionStart;
+            textBox_output.Clear();
             timer.Stop();
             time = 0;
             timer.Start();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            textBox_input.Text =
-@"using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Reflection;
-using System.CodeDom.Compiler;
-using Microsoft.CSharp;
-using System.IO;
-namespace Lab6 //Скрыть от пользователя
-   {  //Сюда написать интерфейс(похожий на Console) и класс который его реализует (UI) и скрыть от пользователя
-        public class Program // Тут объявить объект класса, реализующего UI и скрыть от пользователя
-        {
-            public static void Main() //Это должен видеть пользователь по мнению Илюхи
+            Regex reg = new Regex(@"string |object |int |uint |sbyte |shor |ushor |long |ulong |float |double |char |bool |decimal |public|private|protected|void|static|delegate|enum|new|class\s[A-z,0-9]{1,10}|struct\s[A-z,0-9]{1,10}|interface\s[A-z,0-9]{1,10}|const");
+            MatchCollection mycol = reg.Matches(richTextBox1.Text);
+            if (mycol.Count != 0)
             {
-
+                foreach (Match m in mycol)
+                {
+                    richTextBox1.SelectionStart = m.Index;
+                    richTextBox1.SelectionLength = m.Length;
+                    richTextBox1.SelectionColor = Color.Blue;
+                }
+                richTextBox1.SelectionStart = cur;
+                richTextBox1.SelectionLength = 0;
+                richTextBox1.SelectionColor = Color.Black;
             }
-        }
-   }";
-            compilerParams.ReferencedAssemblies.AddRange(dll);//Добавляем библиотеки к параметрам компилятора
         }
     }
 }
