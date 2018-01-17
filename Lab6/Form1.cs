9be0853fe22c20d92f6b9dc434bd3ad0c505cb5a
Смирнов Михаил Андреевич <32768288+Karnoksis@users.noreply.github.com>
2018-01-17 14:28:14 +0300

    +

--- Lab6/Form1.cs
+++ Lab6/Form1.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 using System.Reflection;
 using System.CodeDom.Compiler;
 using Microsoft.CSharp;
+using System.IO;
 
 namespace Lab6
 {
@@ -20,44 +21,42 @@ namespace Lab6
         {
             InitializeComponent();
         }
+        /// <summary>
+        /// Переменная для счета времени, прошедшего после написания кода (для активации компиляции)
+        /// </summary>
         int time;
         string[] dll = new string[]
         {"System.dll",
          "System.Linq.dll",
          "System.Threading.Tasks.dll",
-         "System.Windows.Forms.dll"
+         "System.Windows.Forms.dll",
+         "mscorlib.dll"
         };
 
         static Dictionary<string, string> providerOptions = new Dictionary<string, string>
         {
             {"CompilerVersion", "v4.0"}
         };
+
         CSharpCodeProvider provider = new CSharpCodeProvider(providerOptions); //Создаем объект компилятора
+
         CompilerParameters compilerParams = new CompilerParameters //Задаем параметры компилятору
         {
             GenerateInMemory = true,
             GenerateExecutable = true,
         };
-        
-
-        /// <summary>
-        /// Компилирует
-        /// </summary>
+        StringBuilder sb = new StringBuilder();
         public void Compile()
         {
-
             CompilerResults results = provider.CompileAssemblyFromSource(compilerParams, textBox_input.Text); //Получаем результат исполнения исходного кода при примененных параметрах
-
+            
             #region Отлов ошибок
             if (results.Errors.HasErrors)
             {
-                StringBuilder sb = new StringBuilder();
-
                 foreach (CompilerError error in results.Errors)
                 {
                     sb.AppendLine(String.Format("Error ({0}): {1}", error.ErrorNumber, error.ErrorText));
                 }
-
                 throw new InvalidOperationException(sb.ToString());
             }
 #endregion
@@ -66,6 +65,10 @@ namespace Lab6
             Assembly assembly = results.CompiledAssembly; //Получаем скомпилированную сборку в объект типа Assembly
             Type program = assembly.GetType("Lab6.Program"); //
             MethodInfo main = program.GetMethod("Main"); //
+
+            timer.Stop();
+            time = 0;
+
             main.Invoke(null, null); //Запускаем метод main
         }
 
@@ -74,9 +77,23 @@ namespace Lab6
             time++;
             if (time >= 3)
             {
-                Compile();
-                timer.Stop();
-                time = 0;
+                try
+                {
+                    Compile();
+                    // var sw = new StringWriter();
+                    // TextWriter sww = Console.Out;
+                    //  Console.SetOut(sw);
+                    // Console.SetError(sw);
+                    textBox_output.Text = Console.Out.ToString();
+                }
+                catch
+                {
+                    timer.Stop();
+                    time = 0;
+                    textBox_output.Clear();                 
+                    textBox_output.Text = sb.ToString();
+                    sb.Clear();
+                }
             }
         }
 
@@ -90,11 +107,11 @@ namespace Lab6
         private void Form1_Load(object sender, EventArgs e)
         {
             textBox_input.Text =
-@"namespace Lab6
-   {
-        public class Program
+@"namespace Lab6 //Скрыть от пользователя
+   {  //Сюда написать интерфейс(похожий на Console) и класс который его реализует (UI) и скрыть от пользователя
+        public class Program // Тут объявить объект класса, реализующего UI и скрыть от пользователя
         {
-            public static void Main()
+            public static void Main() //Это должен видеть пользователь по мнению Илюхи
             {
 
             }
