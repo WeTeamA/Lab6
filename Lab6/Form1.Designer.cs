a9795803d3bd5b87335a14437bec494c78b64838
cTu4 <cTu4_22@users.noreply.github.com>
2018-01-20 20:28:59 +0300

    sd

--- Lab6/Form1.Designer.cs
+++ Lab6/Form1.Designer.cs
@@ -30,15 +30,62 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.textBox_output = new System.Windows.Forms.TextBox();
             this.timer = new System.Windows.Forms.Timer(this.components);
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.richTextBox1 = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.textBox_output = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.richTextBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.richTextBox1.AutoScrollMinSize = new System.Drawing.Size(31, 18);
+            this.richTextBox1.BackBrush = null;
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.richTextBox1.BookmarkColor = System.Drawing.Color.Chartreuse;
+            this.richTextBox1.CharHeight = 18;
+            this.richTextBox1.CharWidth = 10;
+            this.richTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.richTextBox1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.richTextBox1.Font = new System.Drawing.Font("Courier New", 12F);
+            this.richTextBox1.IsReplaceMode = false;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Paddings = new System.Windows.Forms.Padding(0);
+            this.richTextBox1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.richTextBox1.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("richTextBox1.ServiceColors")));
+            this.richTextBox1.Size = new System.Drawing.Size(816, 613);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.WordWrapMode = FastColoredTextBoxNS.WordWrapMode.Custom;
+            this.richTextBox1.Zoom = 100;
+            this.richTextBox1.TextChanged += new System.EventHandler<FastColoredTextBoxNS.TextChangedEventArgs>(this.richTextBox1_TextChanged);
             // 
             // textBox_output
             // 
             this.textBox_output.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.textBox_output.Font = new System.Drawing.Font("Segoe MDL2 Assets", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_output.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox_output.Location = new System.Drawing.Point(12, 631);
             this.textBox_output.Multiline = true;
             this.textBox_output.Name = "textBox_output";
@@ -46,24 +93,6 @@
             this.textBox_output.ScrollBars = System.Windows.Forms.ScrollBars.Both;
             this.textBox_output.Size = new System.Drawing.Size(816, 158);
             this.textBox_output.TabIndex = 2;
-            // 
-            // timer
-            // 
-            this.timer.Interval = 1000;
-            this.timer.Tick += new System.EventHandler(this.timer_Tick);
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox1.ForeColor = System.Drawing.Color.Black;
-            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.richTextBox1.Size = new System.Drawing.Size(816, 577);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // Form1
             // 
@@ -77,15 +106,18 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.richTextBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.TextBox textBox_output;
         private System.Windows.Forms.Timer timer;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private FastColoredTextBoxNS.FastColoredTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox_output;
     }
 }
 
--- Lab6/Form1.cs
+++ Lab6/Form1.cs
@@ -13,6 +13,7 @@ using System.CodeDom.Compiler;
 using Microsoft.CSharp;
 using System.IO;
 using System.Text.RegularExpressions;
+using FastColoredTextBoxNS;
 
 namespace Lab6
 {
@@ -22,8 +23,7 @@ namespace Lab6
         {
             InitializeComponent();
         }
-
-            int sec;
+        int sec;
         string[] dll = new string[]
         {"System.dll",
 "System.Windows.Forms.dll",
@@ -38,9 +38,9 @@ namespace Lab6
 {"CompilerVersion", "v4.0"}
 };
 
-        CSharpCodeProvider provider = new CSharpCodeProvider(providerOptions); 
+        CSharpCodeProvider provider = new CSharpCodeProvider(providerOptions);
 
-        CompilerParameters compilerParams = new CompilerParameters  
+        CompilerParameters compilerParams = new CompilerParameters
         {
             GenerateInMemory = true,
             GenerateExecutable = false
@@ -48,8 +48,8 @@ namespace Lab6
         StringBuilder strb = new StringBuilder();
         public void Run()
         {
-            CompilerResults results = provider.CompileAssemblyFromSource(compilerParams, compil + richTextBox1.Text + "}}"); 
-
+            string text = richTextBox1.Text;
+            CompilerResults results = provider.CompileAssemblyFromSource(compilerParams, compil+ getStr(ref text)[1] +"public void Main(){" + getStr(ref text)[0]+"}}}");
             if (results.Errors.HasErrors)
             {
                 foreach (CompilerError error in results.Errors)
@@ -58,10 +58,10 @@ namespace Lab6
                 }
                 throw new InvalidOperationException(strb.ToString());
             }
- 
-            Assembly assembly = results.CompiledAssembly; 
-            Type program = assembly.GetType("Lab6.Program"); 
-            MethodInfo method = program.GetMethod("Main");  
+
+            Assembly assembly = results.CompiledAssembly;
+            Type program = assembly.GetType("Lab6.Program");
+            MethodInfo method = program.GetMethod("Main");
 
             timer.Stop();
             sec = 0;
@@ -79,13 +79,13 @@ namespace Lab6
         private void timer_Tick(object sender, EventArgs e)
         {
             sec++;
-            if (sec >= 2)
+            if (sec >= 4)
             {
                 try
                 {
                     using (StringWriter stringW = new StringWriter())
                     {
-                       Console.SetOut(stringW);
+                        Console.SetOut(stringW);
                         Run();
 
                     }
@@ -100,6 +100,41 @@ namespace Lab6
                     strb.Clear();
                 }
             }
+        
+        }
+        public string[] getStr(ref string block)
+        {
+            string[] result = new string[2];
+            string p = block;
+
+            Regex outside = new Regex("class|void|struct|interface|delegate");
+            MatchCollection mycollection = outside.Matches(block);
+            foreach (Match m in mycollection)
+            {
+                int check = 0;
+                int k = 0;
+                for (int i = m.Index; i <= block.Length - 1; i++)
+                {
+                    switch (block[i])
+                    {
+                        case '{':
+                            check++;
+                            break;
+                        case '}':
+                            check--;
+                            k ++;
+                            break;
+                    }
+                    if ((check==0)&&(k>0))
+                    {
+                        result[1] += block.Substring(m.Index, i-m.Index+1);
+                        p=p.Replace(block.Substring(m.Index, i - m.Index + 1), "");
+                        break;
+                    }
+                }
+            }
+            result[0] = p;
+            return result;
         }
 
         string compil = @"using System; 
@@ -118,50 +153,56 @@ using Microsoft.CSharp;
 using System.IO; 
 namespace Lab6 
 { 
-public class Program 
-{ 
-interface OutPut 
-{ 
-void Out(string x); 
-} 
-public class WriteText:OutPut 
-{ 
-public  void Out(string x) 
-{
-Console.WriteLine(x); 
-}
-}";
+    public class Program 
+    { 
+        interface OutPut 
+        { 
+        void Out(string x); 
+        } 
+        public class WriteText:OutPut 
+        { 
+            public void Out(string x) 
+                {
+                Console.WriteLine(x); 
+                }
+        }
+
+";
         private void Form1_Load(object sender, EventArgs e)
         {
-            richTextBox1.Text =
-            @"public void Main() 
-{ 
-WriteText s = new WriteText(); // s.Out(string) выведет строку в TextBox 
-
-}";
-            compilerParams.ReferencedAssemblies.AddRange(dll); 
+            compilerParams.ReferencedAssemblies.AddRange(dll);
         }
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        /* private void richTextBox1_TextChanged(object sender, EventArgs e)
+         {
+            // int cur = richTextBox1.SelectionStart;
+             textBox_output.Clear();
+             timer.Stop();
+             sec = 0;
+             timer.Start();
+            /* Regex reg = new Regex(@"string |WriteText|object |int |uint |sbyte |shor |ushor |long |ulong |float |double |char |bool |decimal |public|private|protected|void|static|delegate|enum|new|class\s[A-z,0-9]{1,10}|struct\s[A-z,0-9]{1,10}|interface\s[A-z,0-9]{1,10}|const");
+             MatchCollection mycol = reg.Matches(richTextBox1.Text);
+             if (mycol.Count != 0)
+             {
+                 foreach (Match m in mycol)
+                 {
+                     richTextBox1.SelectionStart = m.Index;
+                     richTextBox1.SelectionLength = m.Length;
+                     richTextBox1.SelectionColor = Color.Blue;
+                 }
+                 richTextBox1.SelectionStart = cur;
+                 richTextBox1.SelectionLength = 0;
+                 richTextBox1.SelectionColor = Color.Black;
+             }
+         }*/
+        Style BlueStyle = new TextStyle(Brushes.Blue, null, FontStyle.Bold);
+        private void richTextBox1_TextChanged(object sender, TextChangedEventArgs e)
         {
-            int cur = richTextBox1.SelectionStart;
+            e.ChangedRange.SetStyle(BlueStyle, @"class|void|struct|interface|delegate", RegexOptions.Multiline);
             textBox_output.Clear();
             timer.Stop();
             sec = 0;
             timer.Start();
-            Regex reg = new Regex(@"string |WriteText|object |int |uint |sbyte |shor |ushor |long |ulong |float |double |char |bool |decimal |public|private|protected|void|static|delegate|enum|new|class\s[A-z,0-9]{1,10}|struct\s[A-z,0-9]{1,10}|interface\s[A-z,0-9]{1,10}|const");
-            MatchCollection mycol = reg.Matches(richTextBox1.Text);
-            if (mycol.Count != 0)
-            {
-                foreach (Match m in mycol)
-                {
-                    richTextBox1.SelectionStart = m.Index;
-                    richTextBox1.SelectionLength = m.Length;
-                    richTextBox1.SelectionColor = Color.Blue;
-                }
-                richTextBox1.SelectionStart = cur;
-                richTextBox1.SelectionLength = 0;
-                richTextBox1.SelectionColor = Color.Black;
-            }
+
         }
     }
 }
